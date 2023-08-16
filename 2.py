8e832444a45ad9336ffa76d81d69b5a673907c10
Bob <137697160+CryptoB4ddy@users.noreply.github.com>
2023-08-16 06:29:45 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -13,5 +13,6 @@ def main():
             print(f"The factorial of {num} is: {result}")
     except ValueError:
         print("Invalid input. Please enter a valid integer.")
+
 if __name__ == "__main__":
     main()
